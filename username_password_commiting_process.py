fd72d98071584a7bfbea5586e7cb6ad9a7512e91
Rithic Hariharan <60564768+gr8rithic@users.noreply.github.com>
2021-10-02 09:55:50 +0530

    Update username_password_commiting_process.py

--- username_password_commiting_process.py
+++ username_password_commiting_process.py
@@ -2,7 +2,7 @@ import mysql.connector
 mydb = mysql.connector.connect(
 	host = "localhost",
 	user = "root",
-	password = "Rithic@2002",
+	password = "password",
 	database = "username_password"
 	)
 
